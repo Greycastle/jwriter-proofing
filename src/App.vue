564fdf1bcc359998528926e2a50a8e78fe4584fa
David <ddikman@gmail.com>
2022-10-20 01:04:19 +0900

    You can now edit and copy

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,65 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from 'vue'
+
+import Text from './components/Text.vue';
+
+const english = ref(`Furikake every morning
+
+I eat rice every day since I moved to Japan. Rice and miso soup. I think this is very normal.
+
+However, if it is just rice, I feel a bit sad. Rice is tasty, but I want a bit more flavour.
+
+For this reason, we eat furikake on top of the rice. Furikake is something you put on top of the rice. There are many different types of furikake, but my preference is Mishima furikake. This furikake is made from sesame seeds, dried eggs and fish.
+
+I think rice and miso soup is a very healthy breakfast. Please give it a try.`)
+
+const original = ref(`毎朝のふりかけ
+
+私は日本に引っ越してから毎日お米を食べます。お米と味噌汁。これはとても普通と思います。
+
+しかし、お米だけだと、ちょっと寂しいです。お米は美味しいですが、もうちょっと味が欲しいですね。
+
+そのために、お米の上にふりかけをかけて食べます。ふりかけはご飯の上に乗せるものです。ふりかけは色々な種類がありますが、私のお好みはMishimaのふりかけです。このふりかけはごま、乾いた卵と魚で作られています。
+
+お米と味噌汁はとても健康な朝ご飯だと思います。ぜひ試してみてください。`)
+
+const proofed = ref(`毎朝のふりかけ
+
+私は日本に引っ越してから毎日お米を食べます。お米と味噌汁。これはとても普通だと思います。
+
+しかし、お米だけだと、ちょっと寂しいです。お米は美味しいですが、もうちょっと味が欲しいですね。
+
+なので、お米の上にふりかけをかけて食べます。ふりかけはご飯の上に乗せるものです。ふりかけは色々な種類がありますが、私のお好みはMishimaのふりかけです。このふりかけは、ごま、乾燥させた卵と魚で作られています。
+
+お米と味噌汁はとても健康的な朝ご飯だと思います。ぜひ試してみてください。`)
+
+const selectedParagraph = ref(-1)
+
+function updated(text: string) {
+  proofed.value = text
+  console.log('Updated', text)
+}
+
+function selectParagraph(index: number) {
+  selectedParagraph.value = index
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="parts">
+    <Text :text="english" :selectedParagraph="selectedParagraph"></Text>
+    <Text :text="proofed" :original="original" :selectedParagraph="selectedParagraph"></Text>
+    <Text :text="proofed" v-on:updated="updated" v-on:selected="selectParagraph"></Text>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.parts {
+  display: flex;
+  flex-direction: row;
+  gap: 32px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/Text.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { diffChars } from 'diff'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  original: {
+    type: String,
+    required: false
+  },
+  selectedParagraph: {
+    type: Number,
+    required: false
+  }
+})
+
+const paragraphs = computed(() => {
+  const ps = props.text.split('\n\n')
+  if (props.original) {
+    const originalPs = props.original.split('\n\n')
+    const newPs = new Array<string>()
+    for (let l = 0; l < ps.length; ++l) {
+      const diff = diffChars(originalPs[l], ps[l])
+      newPs.push(diff)
+    }
+    return newPs
+  }
+  return ps
+})
+
+function isString(obj: any): boolean {
+  return typeof obj === 'string'
+}
+
+function getClass(part: any) : string {
+  if (part.removed) {
+    return 'removed'
+  } else if (part.added) {
+    return 'added'
+  }
+  return 'null'
+}
+
+const emit = defineEmits({
+  selected: (index: number) => true,
+  updated: (text: string) => true
+})
+
+function bindValue(event: Event, index: number) {
+  const target = event.target as HTMLInputElement
+  const text = [ ...paragraphs.value ]
+  text[index] = target.innerText
+  emit('updated', text.join('\n\n'))
+  emit('selected', -1)
+}
+
+const copied = ref(false)
+
+function copy() {
+  navigator.clipboard.writeText(props.text)
+  copied.value = true 
+  setTimeout(() => {
+    copied.value = false
+  }, 500)
+}
+
+</script>
+
+<template>
+  <div class="text-block">
+    <p v-for="content, index of paragraphs" v-bind:class="{ 'seleted': selectedParagraph === index, 'copied': copied }" v:key="content" contenteditable="true" v-on:focus="emit('selected', index)" v-on:blur="bindValue($event, index)">
+      <span v-if="isString(content)">{{ content }}</span>
+      <div v-else>
+        <span v-for="part of content" v:key="part.value" :class="getClass(part)">{{ part.value }}</span>
+      </div>
+    </p>
+    <button v-on:click="copy" :disabled="copied">{{ copied ? 'copied..' : 'copy' }}</button>
+  </div>
+</template>
+
+<style scoped>
+
+.text-block {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.text-block p {
+  text-align: left;
+  padding: 4px 8px;
+}
+
+@keyframes copied {
+  0% {
+    color: auto;
+    transform: translateY(0px);
+  }
+  25% {
+    transform: translateY(5px);
+  }
+  50% {
+    color: rgb(224, 69, 69); 
+    transform: translateY(0px);
+  }
+  75% {
+    transform: translateY(-5px);
+  }
+  100% {
+    color: auto;
+    transform: translateY(0px);
+  }
+}
+
+.text-block p.copied {
+  animation: copied 0.180s;
+}
+
+.text-block p:focus, .text-block p.selected {
+  outline: none;
+  background: rgb(242, 244, 255);
+}
+
+.removed {
+  color: rgb(215, 57, 57);
+  padding: 0 4px;
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+
+.added {
+  background-color: rgb(182, 255, 182);
+  padding: 0 4px;
+
+}
+
+</style>
