fc2ce82639692c7ff5a19aa696c8b4b170321ab1
David <ddikman@gmail.com>
2022-12-12 15:21:30 +0900

    Adding landing page

--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,11 @@ import ProofreadPage from '@/usecases/proofread/ProofreadPage.vue'
 import ReviewPage from '@/usecases/review/ReviewPage.vue'
 import DemoPage from '@/usecases/demo/DemoPage.vue'
 import NotFoundPage from '@/usecases/not-found/NotFoundPage.vue'
+import IndexPage from '@/usecases/home/IndexPage.vue'
 
 const routes = [
-    { path: '/', component: ProofreadPage },
+    { path: '/', component: IndexPage },
+    { path: '/proof-read', component: ProofreadPage },
     { path: '/review/:articleId', component: ReviewPage },
     { path: '/demo', component: DemoPage },
     { path: '/:pathMatch(.*)*', component: NotFoundPage },
